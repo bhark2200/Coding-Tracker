3fab43cfadfe5a117ba509cdd470939176397d64
bhark2200 <bhark2200@gmail.com>
2022-10-11 00:05:21 -0400

    CreateTable Method.  Also getting the app.config connectionstring to work.

---
+++ Coding Tracker/DatabaseAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    internal class DatabaseAccess
+    {
+        internal static string dbConnectionString = ConfigurationManager.ConnectionStrings["CodingTrackerDB"].ConnectionString;
+        internal static void CreateTable()
+        {
+            using (var connection = new SQLiteConnection(dbConnectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS codingTracker (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    StartTime TEXT,
+                    EndTime,
+                    Duration INTEGER
+                    )";
+
+                    tableCmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,24 +1,3 @@
-﻿using System.Configuration;
-using System.Data.SQLite;
+﻿using Coding_Tracker;
 
-ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["CodingTrackerDB"];
-
-using (var connection = new SQLiteConnection(settings))
-    {
-        //Creating the command to be sent to the database
-        using (var tableCmd = connection.CreateCommand())
-        {
-            connection.Open();
-            //Declaring what is that command (in SQL syntax)
-            tableCmd.CommandText =
-                @"CREATE TABLE IF NOT EXISTS codingTracker (
-                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    StartTime TEXT,
-                    EndTime,
-                    Duration INTEGER
-                    )";
-
-            tableCmd.ExecuteNonQuery();
-        }
-    }
-
+DatabaseAccess.CreateTable();
